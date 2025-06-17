0200a11f309e97b359ebbecf5287d78a103275e8
jezekon <78500480+jezekon@users.noreply.github.com>
2025-06-17 16:39:44 +0200

    using modules (instead import functions)

--- src/TopOptEval.jl
+++ src/TopOptEval.jl
@@ -2,12 +2,16 @@ module TopOptEval
 
 # Include submodules
 include("MeshImport/MeshImport.jl")
-include("FiniteElementAnalysis/FiniteElementAnalysis.jl")
-include("ResultsExport/ResultsExport.jl")
-include("Utils/Utils.jl")
+using .MeshImport
 
-# Export submodules for direct access
-export MeshImport, FiniteElementAnalysis, ResultsExport
+include("FiniteElementAnalysis/FiniteElementAnalysis.jl")
+using .FiniteElementAnalysis
+
+include("ResultsExport/ResultsExport.jl")
+using .ResultsExport
+
+include("Utils/Utils.jl")
+using .Utils
 
 # Re-export commonly used functions for convenience
 export import_mesh, create_material_model, setup_problem, 
@@ -20,17 +24,6 @@ export import_mesh, create_material_model, setup_problem,
        solve_system_simp, extract_cell_density
 
 export apply_volume_force!, apply_gravity!, apply_acceleration!, apply_variable_density_volume_force!
-
-# Import specific functions from submodules to re-export
-import .MeshImport: import_mesh, extract_cell_density
-import .FiniteElementAnalysis: create_material_model, setup_problem,
-       assemble_stiffness_matrix!, select_nodes_by_plane,
-       select_nodes_by_circle, apply_fixed_boundary!,
-       apply_sliding_boundary!, apply_force!, solve_system,
-       get_node_dofs, create_simp_material_model, 
-       assemble_stiffness_matrix_simp!, solve_system_simp
-import .ResultsExport: export_results, export_boundary_conditions
-import .Utils: calculate_volume
 
 end # module TopOptEval
 
